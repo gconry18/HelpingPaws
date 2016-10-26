da19b79bc6b931c2bf2faf81e5ea2841073ad0bf
gconry18 <garethc18@gmail.com>
2016-10-26 12:50:16 +0200

    Under construction page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  # root 'shelters#index'
+  root 'home#construction'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
