36459102f991cd22186b93bb5f57d2dd0aa60cf6
gconry18 <garethc18@gmail.com>
2016-10-28 16:13:41 +0200

    Added more to shelter page. Added Annotate - Thanks Bee!

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: shelters
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :string(255)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Shelter < ActiveRecord::Base
 end
